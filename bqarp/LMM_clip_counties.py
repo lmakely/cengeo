ed268125539c2d6515de0ab5a043e7b1ccd923fc
Lauren Makely <lmakely09@gmail.com>
2016-11-02 16:39:36 -0400

    Assessment tools
    Finalizing the assessment tools to work with counties, merge files, use
    any ID (not specific IDs). Removed outdated scripts.

--- bqarp/LMM_clip_counties.py
+++ bqarp/LMM_clip_counties.py
@@ -6,7 +6,7 @@ import logging
 
 
 # Script arguments
-def clip_county_by_selection(census_county_file, local_file, output_folder):
+def clip_county_by_selection(census_county_file, local_file, output_folder, state_code, local_fp_field):
     """
     Goes through a state county file and clips an input file to each county boundary. Used by BQARP to clip local
     data to census data.
@@ -22,11 +22,13 @@ def clip_county_by_selection(census_county_file, local_file, output_folder):
     census_counties_lyr = arcpy.MakeFeatureLayer_management(census_county_file)
 
     logging.info('Beginning to split places by county')
-    with arcpy.da.SearchCursor(census_counties_lyr, field_names=['STATEFP', 'COUNTYFP']) as cursor:
+    field_names = ['{0}'.format(local_fp_field)]
+    with arcpy.da.SearchCursor(census_counties_lyr, field_names) as cursor:
         for row in cursor:
 
-            county_fp = '{0}{1}'.format(row[0], row[1])
-            expression = """ "COUNTYFP" = '{0}' """.format(row[1])
+            # double check with Maddie that this is accurate; their county codes do not already include the state code
+            county_fp = '{0}{1}'.format(state_code, row[0])
+            expression = """ "{0}" = '{1}' """.format(row[0], county_fp)
 
             selection = arcpy.SelectLayerByAttribute_management(census_counties_lyr, "NEW_SELECTION", expression)
             clip_name = 'census_places_{0}.shp'.format(county_fp)
@@ -40,12 +42,14 @@ def clip_county_by_selection(census_county_file, local_file, output_folder):
 
             if len(num_selected_records.fidSet.split(";")) > 0:
                 arcpy.Clip_analysis(local_lyr, selection, output_path)
-                print('Completed clipping: {}'.format(county_fp))
+                print('Completed clipping: {0}'.format(county_fp))
 
     logging.info('Completed splitting places')
 
 if __name__ == "__main__":
-    counties = r'\\batch4.ditd.census.gov\mtdata003_geoarea\BAS\CARP\BQARP\20\Census\bqarp_2016_20_county_v90.shp'
-    places = r'\\batch4.ditd.census.gov\mtdata003_geoarea\BAS\CARP\BQARP\20\SWIM_FILES\KS_20160926.gdb\KS_MunicipalBoundaries'
-    output = r'H:\!!!HDriveStuff\BQARP\20'
-    clip_county_by_selection(counties, places, output)
+    counties = arcpy.GetParameterAsText(0)  # use their county data to clip their places
+    places = arcpy.GetParameterAsText(1)
+    output = arcpy.GetParameterAsText(2)  # file path to state folder where county folder should get made
+    state = arcpy.GetParameterAsText(3)
+    field_name = arcpy.GetParameterAsText(4)
+    clip_county_by_selection(counties, places, output, state, field_name)
