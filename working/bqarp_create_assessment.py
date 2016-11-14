2a5b4da7f22f3600d6a39eba118d61a50c756549
Lauren Makely <lmakely09@gmail.com>
2016-11-14 12:22:47 -0500

    added exception to keep window from closing itself

--- working/bqarp_create_assessment.py
+++ working/bqarp_create_assessment.py
@@ -8,7 +8,7 @@ import winsound
 
 swim = r'\\batch4.ditd.census.gov\mtdata003_geo_SWIM\BQARP\2016'  # this will need to get changed from year to year
 lisrds = r''
-working_dir = r'H:\!!!HDriveStuff\BQARP'  # r'\\batch4.ditd.census.gov\mtdata003_geoarea\BAS\CARP\BQARP'
+working_dir = r'\\batch4.ditd.census.gov\mtdata003_geoarea\BAS\CARP\BQARP'
 template_mxd = r'\\batch4.ditd.census.gov\mtdata003_geoarea\BAS\CARP\BQARP\Tools\Assessment.mxd'
 
 
@@ -83,19 +83,19 @@ def create_and_setup_mxd(template_mxd, where_to_save_mxd, directory_of_features)
                 logger.info('Failed to add {0} to mxd'.format(fc))
                 continue
 
-        mxd_layers = arcpy.mapping.ListLayers(mxd)
+    mxd_layers = arcpy.mapping.ListLayers(mxd)
 
-        for shp in add_these_shps:
-            if shp in mxd_layers:
-                pass
-            else:
-                try:
-                    add_layer = arcpy.mapping.Layer(os.path.join(directory_of_features, shp))
-                    logger.info(shp)
-                    arcpy.mapping.AddLayer(df, add_layer, "AUTO_ARRANGE")
-                except:
-                    logger.info('Failed to add {} to mxd'.format(shp))
-                    continue
+    for shp in add_these_shps:
+        if shp in mxd_layers:
+            pass
+        else:
+            try:
+                add_layer = arcpy.mapping.Layer(os.path.join(directory_of_features, shp))
+                logger.info(shp)
+                arcpy.mapping.AddLayer(df, add_layer, "AUTO_ARRANGE")
+            except:
+                logger.info('Failed to add {} to mxd'.format(shp))
+                continue
 
     output_mxd = os.path.join(where_to_save_mxd, 'BQARP_Assessment.mxd')
     logger.info('\n\nSaving mxd to {}'.format(output_mxd))
@@ -176,34 +176,44 @@ Type its number or enter a list of numbers separated by spaces and press enter.
                 return zip_list
 
 
-# for now, this just gets the ST code to put a temp folder in so the user can move them later or whatever
-# this will also be a permanent directory on the P drive eventually
-state_code = str(input("Which state is being processed? Please enter a two digit state code: "))
-swim = os.path.join(swim, state_code)
-state_dir = os.path.join(working_dir, state_code)
+if __name__ == "__main__":
+    try:
+        # for now, this just gets the ST code to put a temp folder in so the user can move them later or whatever
+        # this will also be a permanent directory on the P drive eventually
+        state_code = str(input("Which state is being processed? Please enter a two digit state code: "))
+        swim = os.path.join(swim, state_code)
+        state_dir = os.path.join(working_dir, state_code)
 
-logging.basicConfig(filename=os.path.join(state_dir, 'log.txt'), level=logging.DEBUG, format='%(message)s', filemode='w')
-logger = logging.getLogger()
-logger.info(make_header('  Processing state {}  '.format(state_code)))
+        logging.basicConfig(filename=os.path.join(state_dir, 'log.txt'), level=logging.DEBUG, format='%(message)s', filemode='w')
+        logger = logging.getLogger()
+        logger.info(make_header('  Processing state {}  '.format(state_code)))
 
-# a couple more directories get formed here
-unzipped_dir = os.path.join(state_dir, 'LOCAL_FILES')
+        # a couple more directories get formed here
+        unzipped_dir = os.path.join(state_dir, 'LOCAL_FILES')
 
-# run: which_zip on a swim/swecs folder (stores a list of values)
-current_zips = list(which_zip(swim))
-logger.info('Unzipping the following to {0}:'.format(unzipped_dir))
+        # run: which_zip on a swim/swecs folder (stores a list of values)
+        current_zips = list(which_zip(swim))
+        logger.info('Unzipping the following to {0}:'.format(unzipped_dir))
 
-# run: extract_zip on chosen zip files. This might take a while.
-for zips in current_zips:
-    logger.info('\n\t{0}'.format(zips))
-    logger.info('\n\n')
-    extract_zip(zips, unzipped_dir)
+        # run: extract_zip on chosen zip files. This might take a while.
+        for zips in current_zips:
+            logger.info('\n\t{0}'.format(zips))
+            logger.info('\n\n')
+            extract_zip(zips, unzipped_dir)
 
-# function?: pull lisrds data like bas does (not currently possible so order it)
-# function: move LISRDS data to workspace or just access it
+        # function?: pull lisrds data like bas does (not currently possible so order it)
+        # function: move LISRDS data to workspace or just access it
 
+        # func: add all data to mxd
+        print('Adding files to mxd...')
+        logger.info(make_header('Adding files to mxd'))
+        create_and_setup_mxd(template_mxd, state_dir, unzipped_dir)
 
-# func: add all data to mxd
-print('Adding files to mxd...')
-logger.info(make_header('Adding files to mxd'))
-create_and_setup_mxd(template_mxd, state_dir, unzipped_dir)
+    except:
+        import sys
+        print sys.exc_info()[0]
+        import traceback
+        print traceback.format_exc()
+    finally:
+        print "Press Enter to continue ..."
+        raw_input()
